94a61eff585948e1d4fbc2abade5f406985acfb5
hazemsaad231 <166450789+hazemsaad231@users.noreply.github.com>
2025-04-10 19:07:51 +0200

    good

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,8 +1,34 @@
 
-const profile = () => {
+const profile = async () => {
+
+   const response = await fetch('https://jsonplaceholder.typicode.com/posts')
+    const data = await response.json()
+
+    console.log(data)
+
+
     return (
-        <div>
-            <h1>Profile</h1>
+        <div className="m-12">
+            <h1 className="text-3xl text-center">Profile</h1>
+            <table className="">
+  <thead>
+    <tr>
+      <th>UserID</th>
+      <th>Title</th>
+      <th>Body</th>
+    </tr>
+  </thead>
+
+  <tbody>
+    {data.map((post: any) => (
+      <tr key={post.id}>
+        <td>{post.userId}</td>
+        <td>{post.title}</td>
+        <td>{post.body}</td>
+      </tr>
+    ))}
+  </tbody>
+</table>
         </div>
     );
 }
